2bdd4e07b32b632f98609a4a69130d01cba728d5
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-02-28 18:38:34 +0300

    remove comments

--- src/router/index.js
+++ src/router/index.js
@@ -12,9 +12,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
@@ -26,6 +23,11 @@ const router = createRouter({
       path: '/check-in-2',
       name: 'check-in-2',
       component: () => import('../views/CheckInTwo.vue')
+    },
+    {
+      path: '/check-out',
+      name: 'check-out',
+      component: () => import('../views/CheckOutView.vue')
     }
   ]
 })
